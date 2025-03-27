910f8fec20452651993bd63a979d3ef128bb2deb
diego chinchilla <diegofch29@hotmail.com>
2025-03-27 07:53:58 -0500

    remove unnesesary package

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,6 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
-import {provideAnimations} from '@angular/platform-browser/animations';
 import { provideNativeDateAdapter } from '@angular/material/core';
 
 export const environment = {
@@ -10,5 +9,5 @@ export const environment = {
 };
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),provideHttpClient(),provideAnimations(),provideNativeDateAdapter() ]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),provideHttpClient(),provideNativeDateAdapter() ]
 };
